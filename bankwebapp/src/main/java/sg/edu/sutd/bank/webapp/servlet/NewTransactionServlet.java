d4a8504276e296f2423f588c7a2de376eef4be6c
Dhaval <Dhaval@Dhavals-MacBook-Pro.local>
2019-10-14 20:24:06 -0500

    first completed financial module

---
+++ bankwebapp/src/main/java/sg/edu/sutd/bank/webapp/servlet/NewTransactionServlet.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2017 SUTD Licensed under the
+	Educational Community License, Version 2.0 (the "License"); you may
+	not use this file except in compliance with the License. You may
+	obtain a copy of the License at
+
+https://opensource.org/licenses/ECL-2.0
+
+	Unless required by applicable law or agreed to in writing,
+	software distributed under the License is distributed on an "AS IS"
+	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+	or implied. See the License for the specific language governing
+	permissions and limitations under the License.
+ */
+
+package sg.edu.sutd.bank.webapp.servlet;
+
+import static sg.edu.sutd.bank.webapp.servlet.ServletPaths.NEW_TRANSACTION;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sg.edu.sutd.bank.webapp.commons.Helper;
+import sg.edu.sutd.bank.webapp.commons.ServiceException;
+import sg.edu.sutd.bank.webapp.model.ClientTransaction;
+import sg.edu.sutd.bank.webapp.model.User;
+import sg.edu.sutd.bank.webapp.service.*;
+import sg.edu.sutd.bank.webapp.model.ClientInfo;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@WebServlet(NEW_TRANSACTION)
+public class NewTransactionServlet extends DefaultServlet {
+	private static final long serialVersionUID = 1L;
+	private ClientTransactionDAO clientTransactionDAO = new ClientTransactionDAOImpl();
+	private TransactionCodesDAO transactionCodesDAO = new TransactionCodesDAOImp();
+
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			ClientTransaction clientTransaction = new ClientTransaction();
+			ClientInfo clientInfo = new ClientInfo();
+			User user = new User(getUserId(req));
+			clientTransaction.setUser(user);
+			clientInfo.setUser(user);
+			BigDecimal amount = new BigDecimal(req.getParameter("amount"));
+			clientTransaction.setAmount(amount);
+
+			//Transcode may be malicious
+			String code = req.getParameter("transcode");
+			code = Helper.input_normalizer(code);
+			if (Helper.xss_match(code)) {
+				throw new ServletException("XSS Injection Attempt");
+			}
+			clientTransaction.setTransCode(code);
+			clientTransaction.setToAccountNum(req.getParameter("toAccountNum"));
+
+			if (transactionCodesDAO.validCode(code,clientInfo.getUser().getId()) && clientTransactionDAO.validTransaction(clientTransaction)) {
+				transactionCodesDAO.updateUsage(code, clientInfo.getUser().getId());
+				clientTransactionDAO.create(clientTransaction);
+				redirect(resp, ServletPaths.CLIENT_DASHBOARD_PAGE);
+			} else {
+				throw new ServletException("Code is invalid or Account does not have enough Balance");
+			}
+		} catch (ServiceException e) {
+			sendError(req, e.getMessage());
+			forward(req, resp);
+		}
+	}
+
+}
